eda6a0a9ec5b541a0a3cca3b2ad31518bf554aeb
sabegomaa261 <sabergomaa261@gmail.com>
2022-08-30 18:15:31 +0200

    Register Validation

--- MVC Project/Models/student.cs
+++ MVC Project/Models/student.cs
@@ -13,33 +13,38 @@ namespace MVC_Project.Models
         [DisplayName("Student ID")]
         public int id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="*")]
         [StringLength(50)]
         public string name { get; set; }
 
+        [Range(15,50 , ErrorMessage = "age must from 15 to 50") ]
         public int? age { get; set; }
 
         [Required]
         public string address { get; set; }
 
-        public int departmentid { get; set; }
 
-        [Required]
+        [RegularExpression("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$" , ErrorMessage ="Invalid Email")]
+        [Required(ErrorMessage ="*")]
         [StringLength(50)]
         public string Email { get; set; }
 
-        [Required]
+        
+        
+        [Required(ErrorMessage ="*")]
         [StringLength(50)]
         public string password { get; set; }
+        [Required(ErrorMessage = "*") ]
 
         [DisplayName("confirm password")]
         [NotMapped]
+        [Compare("password" , ErrorMessage ="not matched password")]
         public string confirm_password { get; set; }
 
 
-        [Required]
+        [Required(ErrorMessage = "*")]
         public string img { get; set; }
-
+        public int departmentid { get; set; }
         public virtual department department { get; set; }
     }
 }

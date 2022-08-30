4c05dc20ac64ec471b4f6f1a672d2141da1a17bc
sabegomaa261 <sabergomaa261@gmail.com>
2022-08-30 22:09:12 +0200

    login

---
+++ MVC Project/Controllers/operationController.cs
@@ -0,0 +1,34 @@
+﻿using MVC_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Project.Controllers
+{
+    public class operationController : Controller
+    {
+        MVCContext context = new MVCContext();
+        public ActionResult login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult login(logindata login)
+        {
+            student s = context.students.Where(l => l.Email == login.email && l.password == login.password).FirstOrDefault();
+
+            if (s != null)
+            {
+                Session.Add("user", s.id);
+                return RedirectToAction("details", "students", new { id = s.id });
+            }
+            else
+            {
+                ViewBag.msg = "incorrect username or password";
+                return View();
+            }
+        }
+    }
+}
---
+++ MVC Project/Models/logindata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Project.Models
+{
+    public class logindata
+    {
+        [RegularExpression("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", ErrorMessage = "Invalid Email")]
+        [Required(ErrorMessage = "*")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string password { get; set; }
+    }
+}

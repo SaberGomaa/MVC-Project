8b26f056ca587ff5fa11292382dcfd760f9d8e29
sabegomaa261 <sabergomaa261@gmail.com>
2022-08-31 15:23:47 +0200

    Ajax and partial view

---
+++ MVC Project/Controllers/studentdeptController.cs
@@ -0,0 +1,30 @@
+﻿using MVC_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Project.Controllers
+{
+    public class studentdeptController : Controller
+    {
+
+        MVCContext context = new MVCContext();
+
+        public ActionResult Index()
+        {
+            SelectList st = new SelectList(context.departments.ToList() , "id" , "name");
+
+            return View(st);
+        }
+
+        public ActionResult studentsindept(int id)
+        {
+            List<student> st = context.students.Where(n=>n.departmentid == id).ToList();
+            ViewBag.st = st;
+            return PartialView();
+        }
+
+    }
+}

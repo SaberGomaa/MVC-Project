7d5cc0916cae633cc008c3f855f76bb86fcabf7a
sabegomaa261 <sabergomaa261@gmail.com>
2022-08-31 13:14:04 +0200

    partial view

---
+++ MVC Project/Controllers/testController.cs
@@ -0,0 +1,21 @@
+﻿using MVC_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Project.Controllers
+{
+    public class testController : Controller
+    {
+        MVCContext context = new MVCContext();
+
+        public ActionResult Index()
+        {
+            List<student> st = context.students.ToList();
+
+            return PartialView(st);
+        }
+    }
+}
